78f256f31fe82d6bb231ccfd3827acf3322334e5
ZenkleusLimited <skoolbod@gmail.com>
2024-09-17 05:01:19 +0100

    feat: prisma init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { StaffModule } from './staff/staff.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, StaffModule],
   controllers: [AppController],
   providers: [AppService],
 })
